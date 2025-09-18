f132e8216ee811262656aa70e0b8685fc8d26099
sleet0922 <sleet0528@outlook.com>
2025-09-18 23:22:16 +0800

    更新 basic.sh

--- basic.sh
+++ basic.sh
@@ -1,4 +1,7 @@
 cp ./sshd_config /etc/ssh/sshd_config
+apt install -y nginx
 cp ./default /etc/nginx/sites-available/default
 systemctl enable nginx
 systemctl start nginx
+systemctl reload nginx
+systemctl restart sshd
