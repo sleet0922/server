6eedd839b7785d1d37701bcf140e6d80efb682b7
sleet0922 <sleet0528@outlook.com>
2026-02-07 04:53:12 +0800

    Update nginx.sh

--- code/nginx.sh
+++ code/nginx.sh
@@ -1,5 +1,8 @@
 apt install -y nginx
 cp ./default /etc/nginx/sites-available/default
+mkdir /ssl
+chmod +x -R 777 /ssl
+cp ./ssl/* /ssl/
 systemctl enable nginx
 systemctl start nginx
 systemctl reload nginx
